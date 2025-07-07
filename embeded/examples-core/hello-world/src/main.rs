db84885c22fc221af6692d4bed91937824800ca3
sammyne <xiangmin-li@qq.com>
2025-07-07 02:48:13 +0000

    feat(embeded/examples-core): demo hello-world

---
+++ embeded/examples-core/hello-world/src/main.rs
@@ -0,0 +1,28 @@
+use std::path::Path;
+use std::rc::Rc;
+
+use libdeno::core::anyhow::{self, Context};
+use libdeno::core::{FsModuleLoader, JsRuntime, PollEventLoopOptions, RuntimeOptions};
+use libdeno::x::url;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let module_path = Path::new("static/app.js");
+
+    let module_url = url::resolve_path(module_path).context("resolve module url")?;
+
+    let opts = RuntimeOptions {
+        module_loader: Some(Rc::new(FsModuleLoader)),
+        ..Default::default()
+    };
+
+    let mut js_runtime = JsRuntime::new(opts);
+
+    let module_id = js_runtime.load_main_es_module(&module_url).await?;
+    let result = js_runtime.mod_evaluate(module_id);
+    js_runtime
+        .run_event_loop(PollEventLoopOptions::default())
+        .await
+        .context("run event-loop")?;
+    result.await.context("eval module")
+}
