07d3a298cf8b587cace1341069e4bbe089e5081a
sammyne <xiangmin-li@qq.com>
2025-07-17 07:53:10 +0000

    feat(embeded/examples-core-extension/pass-v8-args): demo return Option

--- embeded/examples-core-extension/pass-v8-args/cli/static/app.js
+++ embeded/examples-core-extension/pass-v8-args/cli/static/app.js
@@ -8,3 +8,8 @@ console.log(`b = ${b}`);
 
 let ss = lib.string_from_bytes(b);
 console.log(`ss = '${ss}'`);
+
+let some = await lib.return_option(false);
+console.log(`some = ${some}`);
+let none = await lib.return_option(true);
+console.log(`none = ${none}`);
