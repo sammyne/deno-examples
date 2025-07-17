ef324d0d1a588cfdcfb4119c9ca88dce1fc472bd
sammyne <xiangmin-li@qq.com>
2025-07-17 07:53:10 +0000

    feat(embeded/examples-core-extension/pass-v8-args): demo return Option

--- embeded/examples-core-extension/pass-v8-args/ext/src/hello_world.js
+++ embeded/examples-core-extension/pass-v8-args/ext/src/hello_world.js
@@ -1,2 +1,3 @@
 export const string_from_bytes = Deno.core.ops.string_from_bytes; 
 export const string_to_bytes = Deno.core.ops.string_to_bytes; 
+export const return_option = Deno.core.ops.return_option;
--- embeded/examples-core/hello-world/static/app.js
+++ embeded/examples-core/hello-world/static/app.js
@@ -1,1 +1,6 @@
 console.log("Hello World");
+
+const ENCODED = `{"a":1}`
+
+let decoded = JSON.parse(ENCODED);
+console.log(decoded);
